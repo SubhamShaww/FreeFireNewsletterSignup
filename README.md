a68283896c32b7dbd32ee9f918aea6c6414baf96
SubhamShaww <sub.show@gmail.com>
2020-10-22 16:54:09 +0530

    additon of README.md , .gitignore file and changes in app.js

---
+++ README.md
@@ -0,0 +1,4 @@
+**Newsletter Signup Page for Free Fire**
+---
+
+After signing up on this webpage, we get newsletter updates on your email about Free Fire Game updates, gameplay and tips.
--- app.js
+++ app.js
@@ -91,6 +91,6 @@ app.post("/failure", function(req, res){
 
 app.listen(process.env.PORT || 3000, function(){
 
-    console.log("Server is running on port 3000.");
+    console.log("Server is running.");
 
 });
